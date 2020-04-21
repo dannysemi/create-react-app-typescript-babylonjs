81dda1a1ffce5dc9eb5d572084bcb53d009b7fb5
Daniel Semanisin <dsemanisin@gmail.com>
2020-04-21 18:40:32 -0500

    Stripped react and typescript to minimum.

---
+++ public/worker.js
@@ -0,0 +1,26 @@
+importScripts("https://preview.babylonjs.com/babylon.js");
+importScripts("https://preview.babylonjs.com/loaders/babylonjs.loaders.min.js");
+importScripts("/index.js");
+
+console.log("Hello from worker!")
+
+var engine;
+onmessage = function(evt) {
+
+    if (evt.data.canvas) {
+        canvas = evt.data.canvas;
+
+        engine = new BABYLON.Engine(canvas, true);
+        var scene = createScene();
+        
+        engine.runRenderLoop(function() {
+            engine.resize();
+            if (scene.activeCamera) {
+                scene.render();
+            }
+        });
+    } else {
+        canvas.width = evt.data.width;
+        canvas.height = evt.data.height;
+    }
+}
