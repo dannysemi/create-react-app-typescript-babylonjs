82284c3e973a80eeedf4cb0b23864e7712c2767c
Daniel Semanisin <dsemanisin@gmail.com>
2020-04-21 18:59:41 -0500

    Ignoring TS2769 which is definitely a bug.

--- src/App.tsx
+++ src/App.tsx
@@ -6,8 +6,9 @@ class App extends Component<{},{}> {
   componentDidMount = () =>{
     this.canvas.width = this.canvas.clientWidth;
     this.canvas.height = this.canvas.clientHeight;
-    var offscreen = this.canvas.transferControlToOffscreen();
-    var worker = new Worker("/worker.js")
+    var offscreen:OffscreenCanvas = this.canvas.transferControlToOffscreen();
+    var worker:Worker = new Worker("/worker.js");
+    //@ts-ignore
     worker.postMessage({canvas: offscreen},[offscreen]);
   }
   onCanvasLoaded = (c: HTMLCanvasElement) => {
